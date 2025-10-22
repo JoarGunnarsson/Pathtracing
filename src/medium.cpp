bdf2d985f866b90fe7126e089d5af535bb165244
Joar Gunnarsson <joar.gunnarsson@gmail.com>
2025-10-22 19:50:38 +0200

    42: Hotfix for segfault issue.

--- src/medium.cpp
+++ src/medium.cpp
@@ -125,7 +125,7 @@ void MediumStack::pop_medium(const int id) {
     for (int i = stack_size - 1; i >= 0; i--) {
         if (medium_array[i]->id == id) {
             medium_array[i] = nullptr;
-            stack_size -= stack_size - i;
+            stack_size = i;
             return;
         }
     }
