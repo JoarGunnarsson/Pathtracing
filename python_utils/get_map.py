4fda93093b47fc60cda3f741de7dc43b1899e2fd
Joar Gunnarsson <joar.gunnarsson@gmail.com>
2026-02-21 22:51:23 +0100

    80: Updated get_map and README

--- python_utils/get_map.py
+++ python_utils/get_map.py
@@ -6,18 +6,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-MAP_FILE_PATH = pathlib.Path(__file__).parent.parent / "maps"
+PROJECT_PATH = pathlib.Path(__file__).parent.parent
 
 
 class Modes:
     ALBEDO = "albedo"
+    TRANSPARENCY = "transparency"
+    OPACITY = "opacity"
 
 
 def open_file(file_name):
-    file_path = MAP_FILE_PATH / file_name
+    file_path = PROJECT_PATH / file_name
     if not os.path.isfile(file_path):
         raise FileNotFoundError(f"{file_path} does not exist")
     return plt.imread(file_path)
+
+
+def normalize_image(image):
+    if np.max(image) > 1:
+        norm_val = 255
+    else:
+        norm_val = 1
+
+    return image / norm_val
 
 
 def create_albedo_map(input_file, output_file):
@@ -27,21 +38,40 @@ def create_albedo_map(input_file, output_file):
 
     img = np.reshape(img[:, :, 0:dim], (width * height * dim,))
 
-    if np.max(img) > 1:
-        norm_val = 255
-    else:
-        norm_val = 1
-    img = img / norm_val
+    img = normalize_image(img)
     img = np.hstack((np.array([width, height, dim]), img))
 
-    img.tofile(MAP_FILE_PATH / output_file, sep="")
+    img.tofile(PROJECT_PATH / output_file, sep="")
+
+
+def create_1D_map(input_file, output_file, invert):
+    img = open_file(input_file)
+    height, width, dim = img.shape[0], img.shape[1], 1
+    print(f"Creating new 1D map based on file with width: {width} and height: {height}.")
+
+    img = np.max(img[:, :, 0:3], axis=-1)
+    img = img.flatten()
+
+    img = normalize_image(img)
+
+    if invert:
+        img = 1.0 - img
+
+    img = np.hstack((np.array([width, height, dim]), img))
+
+    img.tofile(PROJECT_PATH / output_file, sep="")
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("in_file", help="input file path, relative to maps/")
-    parser.add_argument("out_file", help="output file path, relative to maps/")
-    parser.add_argument("-m", "--mode", default=Modes.ALBEDO, help="which mode to use, available modes are: 'albedo'")
+    parser.add_argument("in_file", help="input file path, relative to the project directory")
+    parser.add_argument("out_file", help="output file path, relative to the project directory")
+    parser.add_argument(
+        "-m",
+        "--mode",
+        default=Modes.ALBEDO,
+        help="which mode to use, available modes are: 'albedo', 'transparency', and 'opacity'",
+    )
     return parser.parse_args()
 
 
@@ -49,6 +79,10 @@ def main():
     args = parse_arguments()
     if args.mode == Modes.ALBEDO:
         create_albedo_map(args.in_file, args.out_file)
+    elif args.mode == Modes.TRANSPARENCY:
+        create_1D_map(args.in_file, args.out_file, invert=False)
+    elif args.mode == Modes.OPACITY:
+        create_1D_map(args.in_file, args.out_file, invert=True)
     else:
         raise ValueError(f"{args.mode} is not a valid mode!")
 
